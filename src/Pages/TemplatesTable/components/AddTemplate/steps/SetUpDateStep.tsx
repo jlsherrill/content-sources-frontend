a0134d4d2e8a75eb7f8d4e43b4df2d7d482a572c
Justin Sherrill <jsherril@redhat.com>
2024-04-05 13:03:06 -0400

    remove console.logs

--- src/Pages/TemplatesTable/components/AddTemplate/steps/SetUpDateStep.tsx
+++ src/Pages/TemplatesTable/components/AddTemplate/steps/SetUpDateStep.tsx
@@ -84,9 +84,6 @@ export default function SetUpDateStep() {
   const columnHeaders = ['Name', /* 'Label',*/ 'Advisories', 'Packages'];
 
   const itemsAfterDate = useMemo(() => data?.data?.filter(({ is_after }) => is_after) || [], [data?.data]);
-  console.log("AFTER")
-  console.log(itemsAfterDate)
-  console.log(data?.data)
   const hasIsAfter = itemsAfterDate.length > 0;
 
   const { isLoading, data: contentData = { data: [], meta: { count: 0, limit: 20, offset: 0 } } } =
